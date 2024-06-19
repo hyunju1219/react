4e3f39e6aa155e3d8d81ce2ce555bfd8d1af01e0
hyunju1219 <kk020123@naver.com>
2024-06-19 17:50:00 +0900

    수정

--- src/ch04/App.js
+++ src/ch04/App.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import Button from "./Button";
+import Input from "./Input";
 
 function App() {
-    const [ number, setNumber ] = useState(0);
+    // <h1>번호: {number}</h1>
+    //     <Button text={"증가"} setNumber={setNumber} />
+    //     <Button text={"감소"} setNumber={setNumber} />
+    const [ str, setStr ] = useState("");
     return <>
-        <h1>번호: {number}</h1>
-        <Button text={"증가"} setNumber={setNumber} />
-        <Button text={"감소"} setNumber={setNumber} />
+        <Input value={ str }/>
+        <Input ph={"입력"} />
     </>
 }
 
