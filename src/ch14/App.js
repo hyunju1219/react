e377b28073969829f331362dc51244673efaebb8
hyunju1219 <kk020123@naver.com>
2024-06-25 16:15:53 +0900

    3

--- src/ch14/App.js
+++ src/ch14/App.js
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
 function App() {
+    const [ imgSrc, setImgSrc ] = useState("");
 
     const handleLoadImgClick = () => {
+
+        //<input type="file" multiple={true} />
         const fileElement = document.createElement("input"); //input 객체 생성
-        fileElement.setAttribute("type", "file");
+        //속성
+        fileElement.setAttribute("type", "file"); //타입을 파일로 지정
         fileElement.setAttribute("multiple", true); //복수선택
-        fileElement.click();
+        fileElement.click(); //위에서 만든 input이 클릭된다.
         //console.log(fileElement.files);
+
         fileElement.onchange = (e) => {
-            console.log(e.target.files);
+            const file = e.target.files[0]; //실제 파일 데이터
+            const filereader = new FileReader(); //데이터 url가져오기 위함
+            filereader.onload = (e) => {
+                setImgSrc(e.target.result);
+            }
+
+            filereader.readAsDataURL(file); // click, 호출되면 onload 실행, 파일의 url 가져옴
         }
 
     }
@@ -16,6 +29,9 @@ function App() {
         <>
         <button onClick={handleLoadImgClick}>이미지 불러오기</button>
         <input type="file" multiple={true} />
+        <div>
+            <img src={imgSrc} alt="" />
+        </div>
         </>
     );
 }
---
+++ src/ch16/App.js
@@ -0,0 +1,18 @@
+ㅑㅡ
+function App() {
+
+
+    return (
+        <>
+            <div>
+                <h1>프로필</h1>
+                <div className=""></div>
+                <input type="text" />
+                <input type="text" />
+                <button>저장</button>
+            </div>
+        </>
+    );
+}
+
+export default App;
